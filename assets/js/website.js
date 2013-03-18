4a5f151a44218f0cd60e23e3299a467feabdacfe
Jonah Wiaderny <jonasch.wiaderny@googlemail.com>
2013-03-18 11:28:02 +0100

    initial commit

---
+++ assets/js/website.js
@@ -0,0 +1,13 @@
+$(document).ready(function initial() {
+
+
+    $('#content div.sep span').click(
+
+    function showcontact() {
+        $('#footer').transition({
+            x: -40
+        });
+        });
+
+        
+    });
